b1b24e775b344dd912290c268898f8cc2731dd08
Tu Nombre <tu-email@example.com>
2026-06-23 08:25:09 +0200

    refactor: delegate Exa collector facade to impl module

--- src/collectors/exa_collector.py
+++ src/collectors/exa_collector.py
@@ -1,3 +1,7 @@
 """Facade for Exa collector implementation."""
 
-from src.collectors.exa_collector_impl import *  # noqa: F401,F403
+import sys
+
+from src.collectors import exa_collector_impl as _impl
+
+sys.modules[__name__] = _impl
